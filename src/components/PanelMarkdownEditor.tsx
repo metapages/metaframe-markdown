c5ab8549b07f8980a736ee1cfe85466b1cae0456
Dion Amago <dion@transition9.com>
2023-10-01 13:45:18 -0700

    Fix inline editing

--- src/components/PanelMarkdownEditor.tsx
+++ src/components/PanelMarkdownEditor.tsx
@@ -1,4 +1,9 @@
-import { useCallback } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 
 import { useMarkdown } from '/@/hooks/useMarkdown';
 
@@ -6,9 +11,20 @@ import { MetaframeInputMap } from '@metapages/metapage';
 import { MetaframeStandaloneComponent } from '@metapages/metapage-embed-react';
 
 export const PanelMarkdownEditor: React.FC = () => {
+  const [markdown, setMarkdown] = useMarkdown();
+  const setOnce = useRef<boolean>(false);
+  const [localText, setLocalText] = useState<string>("");
+  useEffect(() => {
+    if (setOnce.current) {
+      return;
+    }
+    if (!markdown) {
+      return;
+    }
+    setOnce.current = true;
+    setLocalText(markdown);
+  }, [markdown]);
 
-  const [ markdown, setMarkdown] = useMarkdown();
-    
   const onOutputs = useCallback(
     (outputs: MetaframeInputMap) => {
       if (outputs["text"] === undefined || outputs["text"] === null) {
@@ -23,7 +39,7 @@ export const PanelMarkdownEditor: React.FC = () => {
     <div>
       <MetaframeStandaloneComponent
         url="https://editor.mtfm.io/#?button=hidden&menuhidden=true&options=JTdCJTIyYXV0b3NlbmQlMjIlM0F0cnVlJTJDJTIybW9kZSUyMiUzQSUyMm1hcmtkb3duJTIyJTJDJTIyc2F2ZWxvYWRpbmhhc2glMjIlM0F0cnVlJTJDJTIydGhlbWUlMjIlM0ElMjJsaWdodCUyMiU3RA=="
-        inputs={{ text: markdown}}
+        inputs={{ text: localText }}
         onOutputs={onOutputs}
       />
     </div>
