6ed66c5360defc1904aa4f1d88ccadf2878d3892
Dion Amago <dion@transition9.com>
2024-05-08 13:19:05 -0700

    Try this light/dark adjustment

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,10 +1,14 @@
-import { extendTheme, StyleFunctionProps } from "@chakra-ui/react";
-// import { mode } from "@chakra-ui/theme-tools";
+import {
+  extendTheme,
+  StyleFunctionProps,
+} from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
 // example: https://codesandbox.io/s/chakraui-custom-bg-colors-stnyr?file=%2Fsrc%2FApp.js
 // https://github.com/chakra-ui/chakra-ui/discussions/5048
 // one day this will be responsive but for now it's just transparent
 // const dark = "#232323";
+const dark = "#000000";
 // const light = "#f0f0f0";
 const light = "transparent"
 
@@ -13,8 +17,8 @@ export const theme = extendTheme({
     global: (props: StyleFunctionProps) => ({
       body: {
         color: "default",
-        bg: light,
-        // bg: mode(light, dark)(props),
+        // bg: light,
+        bg: mode(light, dark)(props),
       },
     }),
   },
